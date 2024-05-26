2e4ad7625c68217cad98957cea0303af2a2a9421
2i-7 <niina.umekita@gmail.com>
2024-05-26 23:52:39 +0900

    Enterでも送信できる

--- public/script.js
+++ public/script.js
@@ -3,23 +3,35 @@ window.onload = async (event) => {
     const previousWord = await response.text();
     const paragraph = document.querySelector("#previousWord");
     paragraph.innerHTML = `前の単語: ${previousWord}`;
-}
-
-document.querySelector("#nextWordSendButton").onclick = async (event) => {
+  };
+  
+  document.querySelector("#nextWordSendButton").onclick = async (event) => {
+    await sendNextWord();
+  };
+  
+  document.querySelector("#nextWordInput").onkeydown = async (event) => {
+    if (event.key === "Enter") {
+      event.preventDefault(); // デフォルトのEnterキーの動作を防ぐ
+      await sendNextWord();
+    }
+  };
+  
+  async function sendNextWord() {
     const nextWordInput = document.querySelector("#nextWordInput");
     const nextWordInputText = nextWordInput.value;
-
+  
     const response = await fetch(
-        "/shiritori",
-        {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ nextWord: nextWordInputText })
-        }
+      "/shiritori",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ nextWord: nextWordInputText })
+      }
     );
-
+  
     const previousWord = await response.text();
     const paragraph = document.querySelector("#previousWord");
     paragraph.innerHTML = `前の単語: ${previousWord}`;
     nextWordInput.value = "";
-}
+  }
+  
