fa3869f2a7c583869d7e5d20768fd2a8df291f0b
2i-7 <niina.umekita@gmail.com>
2024-05-26 16:06:35 +0900

    しりとり(エラーなし)

--- public/script.js
+++ public/script.js
@@ -1,37 +1,26 @@
-window.onload = async (event) => {
+
+window.onload = async(event) => {
     const response = await fetch("/shiritori", {method: "GET"});
     const previousWord = await response.text();
     const paragraph = document.querySelector("#previousWord");
     paragraph.innerHTML = `前の単語: ${previousWord}`;
-}
-
-document.querySelector("#nextWordSendButton").onclick = async(event) => {
+  }
+  
+  document.querySelector("#newWordSendButton").onclick = async(event) => {
     const nextWordInput = document.querySelector("#nextWordInput");
     const nextWordInputText = nextWordInput.value;
+  
     const response = await fetch(
-        "/shiritori",
-        {
-            method: "POST",
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify({ nextWord: nextWordInputText })
-        }
+      "/shiritori",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ nextWord: nextWordInputText })
+      }
     );
-
-    if (response.status !== 200) {
-        const errorJson = await response.text();
-        const errorObj = JSON.parse(errorJson);
-        if (errorObj["errorCode"] >= 2000){
-            alert("アウトだぜ");
-        }
-        else{
-            alert("続けてみなよ");
-        }
-    }
-    else{
-        const previousWord = await response.text();
-
-        const paragraph = document.querySelector("#previousWord");
-        paragraph.innerHTML = `前の単語: ${previousWord}`;
-        nextWordInput.value = ""
-    }
-}
+  
+    const previousWord = await response.text();
+    const paragraph = document.querySelector("#previousWord");
+    paragraph.innerHTML = `前の単語: ${previousWord}`;
+    nextWordInput.value = "";
+  }
--- server.js
+++ server.js
@@ -1,71 +1,36 @@
-import { serveDir } from
-"https://deno.land/std@0.223.0/http/file_server.ts";
+import { serveDir } from "https://deno.land/std@0.223.0/http/file_server.ts"
 
 let previousWord = "しりとり";
-let wordList = [previousWord];
 
 Deno.serve(async (request) => {
     const pathname = new URL(request.url).pathname;
-    console.log(`pathname: ${pathname}`);
+    console.log(`pathname:${pathname}`);
 
-    if (request.method === "GET" && pathname === "/shiritori"){
+    //くれと言われた
+    if(request.method === "GET" && pathname === "/shiritori"){
         return new Response(previousWord);
     }
 
-    if (request.method === "POST" && pathname === "/shiritori"){
+    //やると言われた
+    if(request.method === "POST" && pathname === "/shiritori"){
         const requestJson = await request.json();
         const nextWord = requestJson["nextWord"];
 
-        if(wordList.includes(nextWord)){
-            return new Response(
-                JSON.stringify({
-                    "errorMessage": `「${nextWord}」は既に使用された単語です。別の単語を入力してください`,
-                    "errorCode": "20002"
-                }),
-                {
-                    status: 400,
-                    headers: { "Content-Type": "application/json; charset=utf-8" },
-                }
-            );
-        }
-
-        if (previousWord.slice(-1) === nextWord.slice(0, 1)){
-            if (nextWord.slice(-1) === "ん"){
-                return new Response(
-                    JSON.stringify({
-                        "errorMessage":`「ん」が入力されてしまったので終了します。`,
-                        "errorCode": "20001"
-                    }),
-                    {
-                        status: 400,
-                        headers: { "Content-Type": "application/json; charset=utf-8" },
-                    } 
-                );
-            }
-            previousWord = nextWord;
-            wordList.push(nextWord);
-        }
-        else {
-            return new Response(
-                JSON.stringify({
-                    "errorMessage":`「${previousWord.slice(-1)}」から始まる言葉を入力してください。`,
-                    "errorCode": "10001"
-                }),
-                {
-                    status: 400,
-                    headers: { "Content-Type": "application/json; charset=utf-8" },
-                }
-            );
+        if(previousWord.slice(-1) === nextWord.slice(0, 1)) {
+        previousWord = nextWord;
         }
 
         return new Response(previousWord);
     }
+
+    //./public以下のファイルは静的ファイルとして公開
     return serveDir(
         request,
         {
             fsRoot:"./public",
-            urlRoot: "",
+            urlRoot:"",
             enableCors: true,
         }
     );
+
 });
